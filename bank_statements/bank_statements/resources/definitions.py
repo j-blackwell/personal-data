27d2f5779a8651bddebbf8bcde25469e645e3517
James Blackwell <33688964+j-blackwell@users.noreply.github.com>
2025-02-17 13:34:26 +0000

    feat: use dagster-db io manager

--- bank_statements/bank_statements/resources/definitions.py
+++ bank_statements/bank_statements/resources/definitions.py
@@ -1,11 +1,14 @@
 import dagster as dg
+from dagster_db import build_custom_duckdb_io_manager
+
 from bank_statements.helpers.settings import SETTINGS
-from bank_statements.resources.duckdb_io_manager import MyDuckDBIOManager
 from bank_statements.resources.pdf_parser import PdfStatementParser
 
 resources = {
     "statement_parser": PdfStatementParser(
         dir_prefix=f"{SETTINGS.bank_statement_dir}/sainsburys"
     ),
-    "io_manager": MyDuckDBIOManager(database=SETTINGS.duckdb)
+    "io_manager": build_custom_duckdb_io_manager().configured(
+        {"database": SETTINGS.duckdb}
+    ),
 }
